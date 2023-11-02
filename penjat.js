cca89aebb1b098d12515b6a3605d2651f94bc958
angel_ivanov <angelspsv@gmail.com>
2023-11-02 18:56:58 +0100

    menú d'inici

---
+++ penjat.js
@@ -0,0 +1,7 @@
+function ElPenjat(){
+    console.log("1. Iniciar un joc" + '\n' + "2. Estadístiques" + '\n' + "3. Sortir");
+    
+    let num = parseInt(prompt("Tria el número de l'opció que vols:"));
+
+
+}
