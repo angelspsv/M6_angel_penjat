32a6a64a7cd4d83bdb2fa29ac16bc071e77b450a
angel_ivanov <angelspsv@gmail.com>
2023-11-03 10:15:07 +0100

    letter loop done

--- penjat.js
+++ penjat.js
@@ -7,7 +7,39 @@ function ElPenjat(){
         num = parseInt(prompt("Tria el número de l'opció que vols:"));
 
         if (num == 1) {
-            console.log("Comença el joc");
+            let entrada = prompt("Entra paraula").toLowerCase(); 
+            let longitud = entrada.length;
+            let mot_espais = "";    
+            
+            //mot amb espais en cop de lletres
+            for (let i=0; i<longitud; i++){
+                mot_espais = mot_espais + " _";
+            }
+            console.log(mot_espais);
+
+            //demanem una lletra a l'usuari
+            let lletra = "m";
+            let cerca_lletra = lletra.match(/[abcdefghijklmnoprstqwyñçvxz]/g);
+            while ((lletra.length == 1) && (cerca_lletra)){
+                lletra = prompt("Entra una lletra:").toLowerCase();
+                cerca_lletra = lletra.match(/[abcdefghijklmnoprstqwyñçvxz]/g);
+                if ((lletra.length == 1) && (cerca_lletra)) {
+                    console.log("Es una lletra\n " + lletra);
+                    return;
+
+                } else {
+                    console.log("no es una lletra");
+                }
+                
+            }
+            
+
+            //let result = text.match(/[aeiou]/g);
+
+
+
+
+            
         } else if (num == 2) {
             console.log("Entra a estadístiques");
 
